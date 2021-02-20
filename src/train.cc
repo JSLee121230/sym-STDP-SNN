3313b2238a82b14f68526f1c5083ce63087bcc99
jslee1225 <jslee@digitalinsights.co.kr>
2021-02-20 23:03:02 +0900

    fix

--- src/train.cc
+++ src/train.cc
@@ -197,9 +197,9 @@ int main()
 #endif
 
       fill_n(testDataEvaluateModePCla, NCla, testDataEvaluateMode);
-      #fill_n(testDataEvaluateModeC2E, size_gC2E, testDataEvaluateMode);
+      //fill_n(testDataEvaluateModeC2E, size_gC2E, testDataEvaluateMode);
       CHECK_CUDA_ERRORS(cudaMemcpy(d_testDataEvaluateModePCla, testDataEvaluateModePCla, NCla * sizeof(bool), cudaMemcpyHostToDevice));
-      #CHECK_CUDA_ERRORS(cudaMemcpy(d_testDataEvaluateModeC2E, testDataEvaluateModeC2E, size_gC2E * sizeof(bool), cudaMemcpyHostToDevice));
+      //CHECK_CUDA_ERRORS(cudaMemcpy(d_testDataEvaluateModeC2E, testDataEvaluateModeC2E, size_gC2E * sizeof(bool), cudaMemcpyHostToDevice));
 
       if (!testDataEvaluateMode) //'false' means run the test set. 'true' means start running test set
       {
