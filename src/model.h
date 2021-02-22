327dbf347bd5ca9025139735b6b8ffdb3666d953
JongseokLee <jslee@digitalinsights.co.kr>
2021-02-22 23:53:10 +0900

    n1600_3

--- src/model.h
+++ src/model.h
@@ -61,12 +61,12 @@
 // #define Exc_Ttheta 1e7 * 2
 // #define Exc_theta_plus 0.05
 /***parameters of 1600 excitatory neurons for mnist***/
-// #define Exc_Ttheta 1e7 * 0.8
-// #define Exc_theta_plus 0.05 * 1.4
+#define Exc_Ttheta 1e7 * 0.8
+#define Exc_theta_plus 0.05 * 1.4
 /***parameters of 400 excitatory neurons for mnist***/
 /***parameters of 100 excitatory neurons for mnist***/
-#define Exc_Ttheta 1e7 * 0.6
-#define Exc_theta_plus 0.07
+// #define Exc_Ttheta 1e7 * 0.6
+// #define Exc_theta_plus 0.07
 /***parameters of 400 excitatory neurons for fashion-mnist***/
 // #define Exc_Ttheta 1e7 * 5
 // #define Exc_theta_plus 0.05
